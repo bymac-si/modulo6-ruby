84fc094ca751def756e3a541eb1d203b25073358
bymac-si <mcastroabarca@gmail.com>
2021-08-30 22:54:48 -0400

    Primer ejercicio ruby

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,15 @@
+puts "¡Hola Mundo!"
+puts ("¿Cómo estás, ¿cuál es tu nombre?")
+
+nombre_usuario = gets
+
+puts "Hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿qué edad tienes?"
+edad=gets.chomp
+puts "tienes #{edad} años"
+
+puts "¿dónde vives? 🏠"
+vivienda = gets.chomp
+puts "¿qué tal #{vivienda}? "
