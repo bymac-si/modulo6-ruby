a73ee369757abb4756c6953e492f423ea28138e0
bymac-si <mcastroabarca@gmail.com>
2021-09-15 19:57:50 -0300

    batalla completa

--- ejercicio5/main.rb
+++ ejercicio5/main.rb
@@ -1,27 +1,26 @@
 require_relative 'monstruo'
 
-monstruo1 = Monstruo.new
-monstruo2 = Monstruo.new
+monstruo3 = Monstruo.new
+monstruo4 = Monstruo.new
 
+puts "BATALLA".center(20, "=")
+puts monstruo3
+puts monstruo4
+puts "Presiones [Enter] para iniciar..."
+gets
 
-puts monstruo1
-puts monstruo2
+puts "INICIO SIMULACIÓN".center(30,"*")
+while monstruo3.vida > 0 and monstruo4.vida > 0
+    sleep(0.5)
+    monstruo3.atacar(monstruo4)
+    sleep(0.5)
+    monstruo4.atacar(monstruo3)
+end
 
-
-# monstruo1 ataca al monstruo2
-# monstruo1.atacar(monstruo2)
-
-while (monstruo1.vidas > 0 and monstruo2.vidas > 0) do
-    monstruo1.atacar(monstruo2)
-    monstruo2.atacar(monstruo1)
-    puts
-    puts "#{monstruo1.nombre} le quedan #{monstruo1.vidas} vidas" 
-    
-    puts "#{monstruo2.nombre} le quedan #{monstruo2.vidas} vidas" 
-    
-    end
-    if monstruo1.vidas < monstruo2.vidas
-        puts "GANO #{monstruo2.nombre}"
-    else
-        puts "GANO!!!!  #{monstruo1.nombre}"
-    end
+puts "FIN DE LA SIMULACIÓN"
+puts "Gano este combate: ... "
+if monstruo3.vida > 0
+    puts monstruo3
+else
+    puts monstruo4
+end
